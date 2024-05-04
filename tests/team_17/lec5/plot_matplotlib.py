86be6f9785bee026fda0ee85834cb58a55e0aa36
yummypenguin <timlin14012005.ee12@nycu.edu.tw>
2024-05-04 16:33:39 +0800

    add new black line marked with x

--- tests/team_17/lec5/plot_matplotlib.py
+++ tests/team_17/lec5/plot_matplotlib.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 x_vals_1 = [1, 2, 3, 4, 5, 6]
 y_vals_1 = [3, 5, 6, 3, 7, 9]
+x_vals_3 = [3, 4, 5, 6, 7, 8]
 plt.plot(x_vals_1, y_vals_1, 'b-', label='first')
 y_vals_2 = [1, 7, 3, 5, 6, 2]
 plt.plot(x_vals_1, y_vals_2, 'r--', label='second')
@@ -9,5 +10,6 @@ x_vals_2 = [1, 10, 9, 7, 3, 5]
 plt.plot(x_vals_2, y_vals_2, 'g-.', label='third')
 plt.plot(x_vals_2, y_vals_1, 'y:', label='fourth')
 plt.plot(x_vals_2, x_vals_2, 'ko', label='fifth')
+plt.plot(x_vals_1, x_vals_3, 'kx', label='sixth')
 plt.legend()
 plt.show()
