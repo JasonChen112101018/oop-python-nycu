8c882738d4ef23071e143b91d8a7d72c5fbabb0e
yummypenguin <timlin14012005.ee12@nycu.edu.tw>
2024-05-04 23:32:52 +0800

    rearrange the code and add a new line

--- tests/team_17/lec5/plot_matplotlib.py
+++ tests/team_17/lec5/plot_matplotlib.py
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt
 
+# x coordinates
 x_vals_1 = [1, 2, 3, 4, 5, 6]
-y_vals_1 = [3, 5, 6, 3, 7, 9]
+x_vals_2 = [3, 5, 6, 3, 7, 9]
 x_vals_3 = [3, 4, 5, 6, 7, 8]
+
+# y coordinates
+y_vals_1 = [1, 7, 3, 5, 6, 2]
+y_vals_2 = [1, 10, 9, 7, 3, 5]
+
+# plot
 plt.plot(x_vals_1, y_vals_1, 'b-', label='first')
-y_vals_2 = [1, 7, 3, 5, 6, 2]
 plt.plot(x_vals_1, y_vals_2, 'r--', label='second')
-x_vals_2 = [1, 10, 9, 7, 3, 5]
-plt.plot(x_vals_2, y_vals_2, 'g-.', label='third')
-plt.plot(x_vals_2, y_vals_1, 'y:', label='fourth')
-plt.plot(x_vals_2, x_vals_2, 'ko', label='fifth')
-plt.plot(x_vals_1, x_vals_3, 'kx', label='sixth')
+plt.plot(x_vals_2, y_vals_1, 'g-.', label='third')
+plt.plot(x_vals_2, y_vals_2, 'y:', label='fourth')
+plt.plot(x_vals_3, y_vals_1, 'k', label='fifth')
+plt.plot(x_vals_3, y_vals_2, 'm+', label='sixth')
+
+# labels
 plt.legend()
 plt.show()
